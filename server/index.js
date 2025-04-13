3b5246d7153ebd254a11015435a7e6a36676139d
macroso <marcogasis01@gmail.com>
2025-04-13 00:06:50 -0600

    Se agrega connection state recovery

--- server/index.js
+++ server/index.js
@@ -1,3 +1,40 @@
 import express from 'express';
+import logger from 'morgan';
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
 
 console.log("log de prueba");
+
+const port = process.env.port ?? 3000;
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    connectionStateRecovery: {}
+});
+
+io.on('connection', (socket) => {
+    console.log('Un usuario se ha conectado!');
+
+    socket.on('disconnect', () => {
+        console.log('Un usuario se ha desconectado!');
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+
+
+
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/client/index.html');
+});
+
+server.listen(port, () =>{
+    console.log(`Servidor corriendo en el puerto ${port}`);
+    
+})
